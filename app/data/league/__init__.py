bb7112056ee493ac5b74548aff113d43b69241ac
Liam Whitenack <liamwhitenack@outlook.com>
2026-01-23 09:50:07 -0500

    saved us some lines

---
+++ app/data/league/__init__.py
@@ -0,0 +1,21 @@
+from app.data.league.awards import Award
+from app.data.league.contract import Contract
+from app.data.league.game import Game
+from app.data.league.payroll import TeamPlayerBuyout, TeamPlayerSalary
+from app.data.league.player import Player, PlayerSeason
+from app.data.league.player_game import PlayerGame
+from app.data.league.prospect import DraftProspect
+from app.data.league.team import Team
+
+__all__ = [
+    "Player",
+    "Team",
+    "Game",
+    "DraftProspect",
+    "Award",
+    "Contract",
+    "PlayerGame",
+    "PlayerSeason",
+    "TeamPlayerSalary",
+    "TeamPlayerBuyout",
+]
