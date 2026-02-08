aac16762160220a04775cbd814e0878ca82dbe40
Liam Whitenack <liamwhitenack@outlook.com>
2026-02-08 16:01:45 -0500

    delay now has a progress bar

--- app/utils/math_utils.py
+++ app/utils/math_utils.py
@@ -1,6 +1,7 @@
 from time import sleep
 
 import numpy as np
+from tqdm import tqdm
 
 
 def delay_seconds_count(mean: float = 5.0, scale: float = 1.0) -> float:
@@ -9,5 +10,8 @@ def delay_seconds_count(mean: float = 5.0, scale: float = 1.0) -> float:
 
 def delay_seconds(shape: float = 5.0, scale: float = 1.0) -> None:
     seconds = np.random.gamma(shape, scale)
-    print(f"delaying {round(seconds, 2)} seconds, mean is {shape * scale}")
-    sleep(seconds)
+    print(f"Delaying {round(seconds, 2)} seconds, mean is {shape * scale}")
+
+    # Show progress bar
+    for _ in tqdm(range(int(seconds * 100)), desc="Waiting", ncols=70):
+        sleep(0.01)
