abb9ef09751682af64dda4de0e5bb55e80bfc946
Liam Whitenack <liamwhitenack@outlook.com>
2026-02-17 20:07:16 -0500

    updated team season salary data

--- app/fill_data/save_to_csv/scrape_spotrac.py
+++ app/fill_data/save_to_csv/scrape_spotrac.py
@@ -101,7 +101,7 @@ def get_all_salary_csvs() -> None:
             )
 
         # Iterate over years and teams
-        for year in range(2027, 2028):
+        for year in range(2010, 2033):
             for _, row in teams.iterrows():
                 team_name: str = row["team_name"]
                 if team_name == "Los Angeles Clippers":
